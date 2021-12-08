2abb0c8d86482b30c349cb9f6d2c0ac8855f1f9f
Pruthvi Lalji <pruthvi_lalji@hotmail.co.uk>
2021-12-08 18:38:27 +0000

    added few validations

--- src/main/java/com/sparta/iomanager/model/util/DataValidation.java
+++ src/main/java/com/sparta/iomanager/model/util/DataValidation.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 public class DataValidation {
 
-
     public static boolean prefixValidation(String prefix){
         return false;
     }
@@ -18,16 +17,23 @@ public class DataValidation {
 
     /* This will be used for both first name and last name*/
     public static boolean nameValidation(String name){
+        String validName = "(^[A-Za-z])((?![ .,'-]$)[a-z .,'-]){0,24}$"; //a string of 24 character is allowed and no number or special character
+        if (name.matches(validName)){
+            return true;
+        }
         return false;
     }
 
 
-    public static boolean initialsValidation(char initial){
+    public static boolean initialsValidation(String initial){
+        String validInitials = "(^[A-Za-z])((?![ .,'-]$)[a-z .,'-]){0,24}$";
+        if (initial == null || initial.isEmpty()) return false;
+        if (initial.matches(validInitials)) return true;
         return false;
     }
 
-    public static boolean genderValidation(char gender){
-        return false;
+    public static boolean genderValidation(String gender){
+    return false;
     }
 
     public static boolean emailValidation(String email){
@@ -36,6 +42,7 @@ public class DataValidation {
 
 
     public static boolean dOBValidation(Date dOB){
+
         return false;
     }
 
