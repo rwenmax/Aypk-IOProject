4cf261f2ae150486312fa25be1c4da8e58a38e14
Pruthvi Lalji <pruthvi_lalji@hotmail.co.uk>
2021-12-08 16:58:23 +0000

    Added class for the data validation along with the methods for all.

---
+++ src/main/java/com/sparta/iomanager/model/util/DataValidation.java
@@ -0,0 +1,51 @@
+package com.sparta.iomanager.model.util;
+
+import java.util.Date;
+
+public class DataValidation {
+
+
+    public static boolean prefixValidation(String prefix){
+        return false;
+    }
+
+
+    /* This could be used for employee id and salary checker*/
+    public static boolean employeeIDValidation(int emp_id){
+        return false;
+    }
+
+
+    /* This will be used for both first name and last name*/
+    public static boolean nameValidation(String name){
+        return false;
+    }
+
+
+    public static boolean initialsValidation(char initial){
+        return false;
+    }
+
+    public static boolean genderValidation(char gender){
+        return false;
+    }
+
+    public static boolean emailValidation(String email){
+        return false;
+    }
+
+
+    public static boolean dOBValidation(Date dOB){
+        return false;
+    }
+
+    public static boolean dateOfBirthValidation(Date dOJ){
+        return false;
+    }
+
+
+
+
+
+
+}
