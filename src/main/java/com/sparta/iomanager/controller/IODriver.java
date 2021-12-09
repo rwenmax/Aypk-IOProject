929756348e02da622f97757baa3404559086ae98
Pruthvi Lalji <pruthvi_lalji@hotmail.co.uk>
2021-12-09 13:13:11 +0000

    Inserting and creating tables

--- src/main/java/com/sparta/iomanager/controller/IODriver.java
+++ src/main/java/com/sparta/iomanager/controller/IODriver.java
@@ -4,10 +4,12 @@ import com.sparta.iomanager.model.Employee;
 import com.sparta.iomanager.view.FileFinder;
 import com.sparta.iomanager.view.Report;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Map;
 
 public class IODriver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, IOException {
         long start = 0;
         long end = 0;
 
@@ -18,7 +20,9 @@ public class IODriver {
         start = System.nanoTime();
         employeeMap = inputManager.insertion(inputManager.readFile(fileFinder.findFile()));
         end = System.nanoTime();
-
+        new EmployeeDaoImpl().dropTable();
+        new EmployeeDaoImpl().createTable();
+        new EmployeeDaoImpl().insertEmployee(employeeMap);
         OutputManager.outPutResults(employeeMap);
 
         System.out.println("Time taken to read file: " + ((end-start)/1000000) + " ms");
