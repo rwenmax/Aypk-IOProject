085bc61da131ebc316af2f772a6c19c9f783bc59
alex <alex@susanu.com>
2021-12-09 16:39:28 +0000

    updates on input manager test

--- src/main/java/com/sparta/iomanager/model/util/Constants.java
+++ src/main/java/com/sparta/iomanager/model/util/Constants.java
@@ -10,6 +10,22 @@ public class Constants {
     public static final String VALID_INITIAL = "(^[A-Za-z])((?![ .,'-]$)[a-z .,'-]){0}$";
     public static final String CHAR_REGEX = "[A-Za-z]+";
     public static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\."+ "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
-    public static final List<String> GENDER = new ArrayList<>(List.of("M", "F"));
+    public static final List<String> GENDER_LIST = new ArrayList<>(List.of("M", "F"));
+    public static final String CSV_HEADER = "Emp ID,Name Prefix,First Name,Middle Initial,Last Name," +
+            "Gender,E Mail,Date of Birth,Date of Joining,Salary";
+    public static final int CSV_LENGTH = 10;
+
+    public static final int ID = 0;
+    public static final int TOC = 1;
+    public static final int FIRST_NAME = 2;
+    public static final int MIDDLE_INITIAL = 3;
+    public static final int LAST_NAME = 4;
+    public static final int GENDER = 5;
+    public static final int EMAIL = 6;
+    public static final int DOB = 7;
+    public static final int DOJ = 8;
+    public static final int SALARY = 9;
+
+    public static final String CSV_SEPARATOR = ",";
 
 }
