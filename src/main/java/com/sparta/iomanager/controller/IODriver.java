8423b196ecc7439d7d08449897a6580788564175
rwenmax <rwenmax@gmail.com>
2021-12-09 11:40:11 +0000

    Added multi-threading to file reading

--- src/main/java/com/sparta/iomanager/controller/IODriver.java
+++ src/main/java/com/sparta/iomanager/controller/IODriver.java
@@ -8,16 +8,20 @@ import java.util.Map;
 
 public class IODriver {
     public static void main(String[] args) {
+        long start = 0;
+        long end = 0;
+
         FileFinder fileFinder = new FileFinder();
 
         InputManager inputManager = new InputManager();
         Map<Integer, Employee> employeeMap;
+        start = System.nanoTime();
         employeeMap = inputManager.insertion(inputManager.readFile(fileFinder.findFile()));
-        for (Map.Entry<Integer, Employee> entry : employeeMap.entrySet()) {
-            System.out.println(entry.getKey() + "/" + entry.getValue().getDob());
-        }
+        end = System.nanoTime();
 
         OutputManager.outPutResults(employeeMap);
+
+        System.out.println("Time taken to read file: " + ((end-start)/1000000) + " ms");
 
         Report.runReport(inputManager);
     }
--- src/main/java/com/sparta/iomanager/controller/InputManager.java
+++ src/main/java/com/sparta/iomanager/controller/InputManager.java
@@ -4,39 +4,87 @@ import com.sparta.iomanager.model.Employee;
 import com.sparta.iomanager.model.util.UtilManager;
 
 import java.io.BufferedReader;
+import java.io.FileDescriptor;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class InputManager {
+public class InputManager{
+    public class ThreadedRead implements Runnable{
+
+        String inFile;
+        int start, end;
+        public ThreadedRead(String inFile, int start, int end){
+            this.inFile = inFile;
+            this.start = start;
+            this.end = end;
+        }
+        @Override
+        public void run() {
+            try (BufferedReader inp = new BufferedReader(new FileReader(inFile))){
+                if (start == 0){
+                    inp.readLine();
+                }
+                for (int i = 0; i < start; i++){
+                    inp.readLine();
+                }
+                String lineOfText;
+                for (int i = start; i < end; i++) {
+                    lineOfText = inp.readLine();
+                    String[] tfields = lineOfText.split(",");
+                    for (int j = 0; j < tfields.length; j++) {
+                        fields[i][j] = tfields[j];
+                    }
+                }
+            } catch (IOException e){
+                System.out.println("Unable to open file!");
+                e.printStackTrace();
+            }
+            threadsFinished++;
+        }
+    }
     final int ID = 0, toc = 1, firstName = 2, middleInitial = 3, lastName = 4, gender = 5,
             email = 6, dob = 7, doj = 8, salary = 9;
     private Map<Integer, Employee> employeeHashMap = new HashMap<>();
     private Map<Integer, Employee> duplicateValues = new HashMap<>();
 
-    public String[][] readFile(String inFile) {
-        String[][] fields = new String[0][0];
-        try (BufferedReader inp = new BufferedReader(new FileReader(inFile))) {
+    private String[][] fields = new String[0][0];
+    private int threadsFinished = 0;
+
+    public String[][] readFile(String inFile){
+        try (BufferedReader inp = new BufferedReader(new FileReader(inFile))){
+            inp.readLine();
             int lines = 0;
             while (inp.readLine() != null) lines++;
-            fields = new String[--lines][10];
-            BufferedReader in = new BufferedReader(new FileReader(inFile));
-            in.readLine();
-            String lineOfText;
-            for (int i = 0; i < lines; i++) {
-                lineOfText = in.readLine();
-                String[] tfields = lineOfText.split(",");
-                for (int j = 0; j < tfields.length; j++) {
-                    fields[i][j] = tfields[j];
-                }
-            }
-            in.close();
-        } catch (IOException e) {
-            //Log the error
-            System.out.println("Unable to open file!");
+            fields = new String[lines][10];
+            int thread1, thread2, thread3;
+
+            thread1 = (int)(lines*0.25);
+            thread2 = (int)(lines*0.5);
+            thread3 = (int)(lines*0.75);
+
+            ThreadedRead threadedRead1 = new ThreadedRead(inFile, 0, thread1);
+            Thread t1 = new Thread(threadedRead1);
+            t1.start();
+
+            ThreadedRead threadedRead2 = new ThreadedRead(inFile, thread1, thread2);
+            Thread t2 = new Thread(threadedRead2);
+            t2.start();
+
+            ThreadedRead threadedRead3 = new ThreadedRead(inFile, thread2, thread3);
+            Thread t3 = new Thread(threadedRead3);
+            t3.start();
+
+            ThreadedRead threadedRead4 = new ThreadedRead(inFile, thread3, lines);
+            Thread t4 = new Thread(threadedRead4);
+            t4.start();
+        } catch (IOException e){
             e.printStackTrace();
+        }
+        while(threadsFinished < 4){
+            System.out.println(threadsFinished);
         }
         return fields;
     }
